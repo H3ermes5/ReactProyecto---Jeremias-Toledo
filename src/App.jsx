30daa93f0ea92b4faf24ad65899e26112f8bfe90
Jere <jeeretoledo@gmail.com>
2024-03-08 15:39:25 -0300

    Creando repositorio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Input } from './components/input'
+
+
+function App() {
+return (
+ <>
+ <h1>HELLO WORLD!🌍</h1>
+ <Input />
+ <Input />
+ <Input />
+
+ </> 
+)
+  
+}
+
+export default App
